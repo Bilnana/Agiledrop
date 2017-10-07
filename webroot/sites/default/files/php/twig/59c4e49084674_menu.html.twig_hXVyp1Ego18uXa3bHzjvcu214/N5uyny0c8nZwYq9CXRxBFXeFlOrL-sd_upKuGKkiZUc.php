adb23571a76d59e410c8aab8bf25a5c80128ed02
Bilja <biljanalj85@gmail.com>
2017-10-07 17:00:06 +0200

    Add changes to all pages responsive.

--- webroot/sites/default/files/php/twig/59c4e49084674_menu.html.twig_hXVyp1Ego18uXa3bHzjvcu214/N5uyny0c8nZwYq9CXRxBFXeFlOrL-sd_upKuGKkiZUc.php
+++ webroot/sites/default/files/php/twig/59c4e49084674_menu.html.twig_hXVyp1Ego18uXa3bHzjvcu214/N5uyny0c8nZwYq9CXRxBFXeFlOrL-sd_upKuGKkiZUc.php
@@ -15,9 +15,9 @@ class __TwigTemplate_d472411c9889345c8c61c4c4e20ad9d8d977cb13fed4d85bc799ce6020c
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $tags = array("import" => 21, "macro" => 29, "if" => 31, "for" => 37, "set" => 39);
+        $tags = array("import" => 22, "macro" => 30, "if" => 32, "for" => 38, "set" => 40);
         $filters = array();
-        $functions = array("link" => 47);
+        $functions = array("link" => 48);
 
         try {
             $this->env->getExtension('sandbox')->checkSecurity(
@@ -39,19 +39,22 @@ class __TwigTemplate_d472411c9889345c8c61c4c4e20ad9d8d977cb13fed4d85bc799ce6020c
             throw $e;
         }
 
-        // line 21
-        $context["menus"] = $this;
-        // line 22
+        // line 1
         echo "
 ";
-        // line 27
+        // line 22
+        $context["menus"] = $this;
+        // line 23
+        echo "
+";
+        // line 28
         echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->renderVar($context["menus"]->getmenu_links((isset($context["items"]) ? $context["items"] : null), (isset($context["attributes"]) ? $context["attributes"] : null), 0)));
         echo "
 
 ";
     }
 
-    // line 29
+    // line 30
     public function getmenu_links($__items__ = null, $__attributes__ = null, $__menu_level__ = null, ...$__varargs__)
     {
         $context = $this->env->mergeGlobals(array(
@@ -65,62 +68,62 @@ class __TwigTemplate_d472411c9889345c8c61c4c4e20ad9d8d977cb13fed4d85bc799ce6020c
 
         ob_start();
         try {
-            // line 30
-            echo "  ";
-            $context["menus"] = $this;
             // line 31
             echo "  ";
+            $context["menus"] = $this;
+            // line 32
+            echo "  ";
             if ((isset($context["items"]) ? $context["items"] : null)) {
-                // line 32
+                // line 33
                 echo "    ";
                 if (((isset($context["menu_level"]) ? $context["menu_level"] : null) == 0)) {
-                    // line 33
+                    // line 34
                     echo "      <ul";
                     echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => "menu"), "method"), "html", null, true));
                     echo ">
     ";
                 } else {
-                    // line 35
+                    // line 36
                     echo "      <ul class=\"menu\">
     ";
                 }
-                // line 37
+                // line 38
                 echo "    ";
                 $context['_parent'] = $context;
                 $context['_seq'] = twig_ensure_traversable((isset($context["items"]) ? $context["items"] : null));
                 foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
-                    // line 38
-                    echo "      ";
                     // line 39
-                    $context["classes"] = array(0 => "menu-item", 1 => (($this->getAttribute(                    // line 41
-$context["item"], "is_expanded", array())) ? ("menu-item--expanded") : ("")), 2 => (($this->getAttribute(                    // line 42
-$context["item"], "is_collapsed", array())) ? ("menu-item--collapsed") : ("")), 3 => (($this->getAttribute(                    // line 43
+                    echo "      ";
+                    // line 40
+                    $context["classes"] = array(0 => "menu-item", 1 => (($this->getAttribute(                    // line 42
+$context["item"], "is_expanded", array())) ? ("menu-item--expanded") : ("")), 2 => (($this->getAttribute(                    // line 43
+$context["item"], "is_collapsed", array())) ? ("menu-item--collapsed") : ("")), 3 => (($this->getAttribute(                    // line 44
 $context["item"], "in_active_trail", array())) ? ("menu-item--active-trail") : ("")));
-                    // line 46
+                    // line 47
                     echo "      <li";
                     echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute($this->getAttribute($context["item"], "attributes", array()), "addClass", array(0 => (isset($context["classes"]) ? $context["classes"] : null)), "method"), "html", null, true));
                     echo ">
         ";
-                    // line 47
+                    // line 48
                     echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->env->getExtension('drupal_core')->getLink($this->getAttribute($context["item"], "title", array()), $this->getAttribute($context["item"], "url", array())), "html", null, true));
                     echo "
         ";
-                    // line 48
+                    // line 49
                     if ($this->getAttribute($context["item"], "below", array())) {
-                        // line 49
+                        // line 50
                         echo "          ";
                         echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->renderVar($context["menus"]->getmenu_links($this->getAttribute($context["item"], "below", array()), (isset($context["attributes"]) ? $context["attributes"] : null), ((isset($context["menu_level"]) ? $context["menu_level"] : null) + 1))));
                         echo "
         ";
                     }
-                    // line 51
+                    // line 52
                     echo "      </li>
     ";
                 }
                 $_parent = $context['_parent'];
                 unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
                 $context = array_intersect_key($context, $_parent) + $_parent;
-                // line 53
+                // line 54
                 echo "    </ul>
   ";
             }
@@ -149,12 +152,13 @@ $context["item"], "in_active_trail", array())) ? ("menu-item--active-trail") : (
 
     public function getDebugInfo()
     {
-        return array (  124 => 53,  117 => 51,  111 => 49,  109 => 48,  105 => 47,  100 => 46,  98 => 43,  97 => 42,  96 => 41,  95 => 39,  93 => 38,  88 => 37,  84 => 35,  78 => 33,  75 => 32,  72 => 31,  69 => 30,  55 => 29,  48 => 27,  45 => 22,  43 => 21,);
+        return array (  127 => 54,  120 => 52,  114 => 50,  112 => 49,  108 => 48,  103 => 47,  101 => 44,  100 => 43,  99 => 42,  98 => 40,  96 => 39,  91 => 38,  87 => 36,  81 => 34,  78 => 33,  75 => 32,  72 => 31,  58 => 30,  51 => 28,  48 => 23,  46 => 22,  43 => 1,);
     }
 
     public function getSource()
     {
-        return "{#
+        return "
+{#
 /**
  * @file
  * Theme override to display a menu.
